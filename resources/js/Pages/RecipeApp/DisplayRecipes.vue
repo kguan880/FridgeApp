6a478bd15d0e8c203e82211dd13cf45089cc2af6
Kevin Guan <kevinguan97@gmail.com>
2020-09-26 18:03:01 +1200

    style displayrecipes component

--- resources/js/Pages/RecipeApp/DisplayRecipes.vue
+++ resources/js/Pages/RecipeApp/DisplayRecipes.vue
@@ -1,8 +1,19 @@
 <template>
-    <div>
-        <div v-for="recipe in getRecipes" :key="recipe.id">
-            <a :href="'/summary/' + recipe.id"><li>{{recipe.title}} - {{recipe.missedIngredientCount}}</li></a>
-            <img :src="recipe.image" alt="">
+    <div class="recipe-grid-display">
+        <div v-for="recipe in getRecipes" :key="recipe.id" class="recipe-item" v-bind:class= "recipe.missedIngredientCount == 0 ? 'grid-colour-green':'grid-colour-red'">     
+            <a :href="/summary/ + recipe.id"><div>
+            <h2>{{recipe.title}}</h2>
+            <img :src="recipe.image" >
+            <div v-if="recipe.missedIngredientCount == 0">
+                <p> You have all the ingredients required to make this recipe! <br> Click here to see how</p>
+            </div>
+            <div v-else-if="recipe.missedIngredientCount !== 0">
+                <p> Here are the ingredients that you are missing for this recipe: <br></p>
+                <ul v-for="ingredient in recipe.missedIngredients" :key="ingredient.id">
+                    <li>{{ingredient.name}}</li>
+                </ul>
+            </div>
+        </div></a>
         </div>
     </div>
 </template>
@@ -17,4 +28,41 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.recipe-grid-display{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.recipe-item{
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    height: 500px;
+    border-radius: 25px;
+    text-align: center;
+    align-items: center;
+    clip-path: inset(0 0 85% 0);
+    transition: clip-path 1s;
+    margin: 5px;
+}
+
+.recipe-item h2{
+    padding-top: 25px;
+    padding-bottom: 50px;
+}
+
+.recipe-item:hover {
+       box-shadow: inset 0 0 80px 80px rgba(255, 255, 255, 0.2);
+       clip-path: inset(0 0 0 0);
+}
+
+.grid-colour-red{
+    background-color: red;
+}
+
+.grid-colour-green{
+    background-color: green;
+}
+</style>
